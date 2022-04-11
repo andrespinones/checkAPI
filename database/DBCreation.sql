a2c8d659c42596ea3c3df4e01ef9e9ea0550b421
Andrés Piñones Besnier <a01570150@itesm.mx>
2022-04-10 20:05:14 -0500

    update db table creation

--- database/DBCreation.sql
+++ database/DBCreation.sql
@@ -4,7 +4,7 @@
 --     baseUrl VARCHAR(2048) NOT NULL,
 --     description VARCHAR(280) NOT NULL,
 --     category VARCHAR(30) NOT NULL,
---     status BIT NOT NULL, --NO BOOL = BIT
+--     status BIT NOT NULL, --BOOL = BIT
 --     sla INT, --SERVICE LEVEL 
 --     latency INT,
 --     isFavorite BIT NOT NULL
@@ -12,17 +12,23 @@
 
 -- CREATE TABLE Tag ( --AGRUPACIONES DE ENDPOINTS
 --     tagID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
---     name VARCHAR(30) NOT NULL,
+--     name VARCHAR(30) NOT NULL
 -- )
 
 -- CREATE TABLE Endpoint (
 --     endpointID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 --     methodType VARCHAR(6) NOT NULL,
 --     path VARCHAR(64) NOT NULL, 
---     description VARCHAR(280) NOT NULL,
+--     endpointDescription VARCHAR(280) NOT NULL,
 --     lastResp INT,
---     status BIT NOT NULL,
+--     status BIT NOT NULL
 -- );
+
+CREATE TABLE ResponseCode (
+  responseCodeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  responseDescription VARCHAR(50),
+  number INT NOT NULL   
+);
 
 --AGREGAR VALORES ANTES DE FKs
 -- ALTER TABLE dbo.Endpoint
