54d0777f9b6f7673cb95a8bbe24dfa158c20a59c
lucas-idigoras278 <62305802+lucas-idigoras278@users.noreply.github.com>
2022-04-21 18:33:25 -0500

    vista de tarjetas datos en arreglo estático

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BlogPostCard } from '../blog-post-card.module';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  data!: BlogPostCard;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
