4193210b217729e5a094f63164c783ddd0933753
lucas-idigoras278 <62305802+lucas-idigoras278@users.noreply.github.com>
2022-04-27 17:47:22 -0500

    estilos de cardView agregados al CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
       {
         mainImageUrl: 'https://images.unsplash.com/photo-1650396693368-1c7239d278ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
         tag: 'tag example',
-        title: 'API2',
+        title: 'API 2',
         description: 'Lorem ipsum dolor sit amet et et invidunt commodo. Dolore duis dignissim labore dolor elitr eirmod blandit stet amet nihil stet amet erat nisl et. No illum delenit sadipscing diam lorem adipiscing eos stet dolores elit vero diam sed ullamcorper ut esse rebum accusam. Eirmod consetetur dia',
         status: false,
         serviceLevel: 20,
@@ -32,7 +32,7 @@ export class AppComponent {
       {
         mainImageUrl: 'https://images.unsplash.com/photo-1650399470902-0dd9259b91c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
         tag: 'tag example',
-        title: 'API3',
+        title: 'API 3',
         description: 'Lorem ipsum dolor sit amet et et invidunt commodo. Dolore duis dignissim labore dolor elitr eirmod blandit stet amet nihil stet amet erat nisl et. No illum delenit sadipscing diam lorem adipiscing eos stet dolores elit vero diam sed ullamcorper ut esse rebum accusam. Eirmod consetetur dia',
         status: true,
         serviceLevel: 75,
@@ -43,7 +43,7 @@ export class AppComponent {
       {
         mainImageUrl: 'https://images.unsplash.com/photo-1650445332429-75ceee3f3226?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
         tag: 'tag example',
-        title: 'API4',
+        title: 'API 4',
         description: 'Lorem ipsum dolor sit amet et et invidunt commodo. Dolore duis dignissim labore dolor elitr eirmod blandit stet amet nihil stet amet erat nisl et. No illum delenit sadipscing diam lorem adipiscing eos stet dolores elit vero diam sed ullamcorper ut esse rebum accusam. Eirmod consetetur dia',
         status: false,
         serviceLevel: 15,
@@ -52,9 +52,9 @@ export class AppComponent {
       },
   
       {
-        mainImageUrl: 'https://images.unsplash.com/photo-1650445332429-75ceee3f3226?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+        mainImageUrl: 'https://images.unsplash.com/photo-1650460230786-5d391175b44b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
         tag: 'tag example',
-        title: 'API5',
+        title: 'API 5',
         description: 'Lorem ipsum dolor sit amet et et invidunt commodo. Dolore duis dignissim labore dolor elitr eirmod blandit stet amet nihil stet amet erat nisl et. No illum delenit sadipscing diam lorem adipiscing eos stet dolores elit vero diam sed ullamcorper ut esse rebum accusam. Eirmod consetetur dia',
         status: false,
         serviceLevel: 15,
