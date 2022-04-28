5e8e45feeeddba5eccd9f71c9cd315b84396b663
Jorge Ayala <A00828633@itesm.mx>
2022-04-28 17:48:50 -0500

    bootstrap and scripst and modules updates
    Co-Authored-By: danielatmz <70904275+danielatmz@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { CategorySidebarComponent } from './components/shared/category-sidebar/category-sidebar.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategorySidebarComponent,
     NavbarComponent,
     HomeComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
