c9b89399d256a59e4cf8aedfbfc1438b64e0b11e
lucas-idigoras278 <62305802+lucas-idigoras278@users.noreply.github.com>
2022-05-03 16:23:19 -0500

    user-list module deletation

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,25 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from './user-list.module';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements AfterViewInit  {
 
+  columnsToDisplay = ['Name', 'Email', "Role"];
+  dataSource = new MatTableDataSource<User>(userArray);
 
-
-  userArray: User[] = [
-    {name: "Pablo", email: "example@gmail.com", role: "admin"},
-    {name: "Daniela", email: "example@gmail.com", role: "admin"},
-    {name: "Andres", email: "example@gmail.com", role: "admin"},
-    {name: "Jorge", email: "example@gmail.com", role: "admin"},
-  ]
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
+
+export interface User{
+  email: string;
+  role: string;
+  name: string;
+}
+
+const userArray: User[] = [
+  {name: "Pablo", email: "example@gmail.com", role: "admin"},
+  {name: "Daniela", email: "example@gmail.com", role: "admin"},
+  {name: "Andres", email: "example@gmail.com", role: "admin"},
+  {name: "Jorge", email: "example@gmail.com", role: "admin"},
+  {name: "Pablo", email: "example@gmail.com", role: "admin"},
+  {name: "Daniela", email: "example@gmail.com", role: "user"},
+  {name: "Andres", email: "example@gmail.com", role: "user"},
+  {name: "Andres", email: "example@gmail.com", role: "user"},
+  {name: "Jorge", email: "example@gmail.com", role: "admin"},
+  {name: "Pablo", email: "example@gmail.com", role: "admin"},
+  {name: "Daniela", email: "example@gmail.com", role: "admin"},
+  {name: "Andres", email: "example@gmail.com", role: "user"},
+];
+
