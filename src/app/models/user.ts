47955c735db3202c98405e7be84743c709f88e45
Andrés Piñones Besnier <a01570150@itesm.mx>
2022-05-19 18:55:34 -0500

    added User class

---
+++ src/app/models/user.ts
@@ -0,0 +1,20 @@
+export class User {
+    constructor(
+        public email: string,
+        public _token: string, 
+        private role: number,
+        private expirationDate: Date
+    ){}
+
+    get token()
+    { 
+        //Regresa el atributo privado _token
+        if( new Date() > this.expirationDate)
+        {
+            //Si el token expira entonces regresa nulo
+            console.log('token expired')
+            return null;
+        }
+        return this._token;
+    }
+}
