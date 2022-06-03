31e7ea8e111ae66e1015f311f06c364f9e9d4500
danielatmz <a01197468@itesm.mx>
2022-06-03 14:21:01 -0500

    Overlay in endpoints-list

--- src/app/components/endpoint-list/endpoint-list.component.ts
+++ src/app/components/endpoint-list/endpoint-list.component.ts
@@ -1,8 +1,12 @@
 import { Endpoint } from './../../models/endpoint';
-import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, ViewContainerRef, TemplateRef, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { endpointSidebar } from 'src/app/models/endpointSidebar';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import { CdkConnectedOverlay} from '@angular/cdk/overlay';
+import {map, filter, startWith, switchMap, delay, mapTo} from 'rxjs/operators';
+import { merge, Observable } from 'rxjs';
 // import { Endpoint } from 'src/app/models/endpoint';
 
 @Component({
@@ -15,62 +19,47 @@ export class EndpointListComponent implements OnInit {
   // prueba con lista local de tipo EP
   DROPDOWN_LIST: EP[];
 
+  apiname = "Petstore";
+  apibaseUrl = "pestore.api"
 
+  showPanel$: boolean =true;
+  @ViewChild(CdkConnectedOverlay, {static: true})
+  private connectedOverlay = CdkConnectedOverlay;
+
+  isPanelVisible: boolean = false;
+  private isPanelHidden: boolean = false;
+  // private isPanelHidden$: Observable<boolean>;
 
 
   @Output() outputToParent = new EventEmitter<Endpoint>();
-  constructor(private service:ApiService, private route: ActivatedRoute) { 
+  constructor(private service:ApiService, private route: ActivatedRoute, private focusMonitor: FocusMonitor) { 
     this.DROPDOWN_LIST = [
-      {
-        type: "GET", path: "/pet/{petID}"
-      },
-      {
-        type: "POST", path: "/pet"
-      },
-      {
-        type: "DELETE", path: "/pet/{petID}"
-      },
-      {
-        type: "PUT", path: "/pet"
-      }
-
+      // {type: "GET", path: "/pet/{petID}"},
+      // {type: "POST", path: "/pet"},
+      // {type: "DELETE", path: "/pet/{petID}"},
+      // {type: "PUT", path: "/pet"}
     ]
-
-
   }
+
+
+  //función del overlay 
+
+
   endpoint:Endpoint = {} as Endpoint;
   testList:any[]=[];
   grouped: { [key: string]: endpointSidebar[] } = {};
   apiID: any;
+
+  toggleDropdown(){
+    this.showPanel$ = false;
+  }
+
   ngOnInit(): void {
     const id = this.route.snapshot.queryParamMap.get('id');
     this.apiID=id;
-    this.getGroups();
-  }
-  getGroups(){
-    this.service.getGroupsbyID(this.apiID).subscribe(resp=>{
-      this.testList = resp;
-      console.log(this.testList)
-      this.grouped = this.testList.reduce((group, current)=> {
-        //create your grouping key, by which you want to group the items
-        const groupingKey = `${current.name}`;
-        //if the group does not yet have an entry for this key, init it to empty array
-        group[groupingKey] = group[groupingKey] || [];
-        //add the current item to the group
-        group[groupingKey].push(current);
-        return group;
-        }, {} ) 
-    })
-  }
+    this.isPanelVisible = true;
 
-  // cambiar al servicio que de el id del grupo para desplegar todos los endpoints
-/*   getEndpointDetail(endpointId:number){
-    this.service.getEndpointbyID(endpointId).subscribe(resp=>{
-      this.endpoint = resp;
-      this.outputToParent.emit(this.endpoint)
-    })
-  } */
-
+  }
 }
 
 export class EP{
