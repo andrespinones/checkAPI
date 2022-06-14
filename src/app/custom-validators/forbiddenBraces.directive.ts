1affeb530f02a64f287d2cc83e9af102dc9527d4
Pablo Cruz <a01138740@itesm.mx>
2022-06-14 13:54:34 -0500

    changed RegEx to Parse function in validator

--- src/app/custom-validators/forbiddenBraces.directive.ts
+++ src/app/custom-validators/forbiddenBraces.directive.ts
@@ -1,9 +1,6 @@
 import { AbstractControl } from '@angular/forms';
 
 export function ValidatePath(control: AbstractControl) {
-
-    // Using ArrayDeque is faster
-    // than using Stack class
     let stack = [];
 
     // Traversing the Expression
@@ -11,39 +8,19 @@ export function ValidatePath(control: AbstractControl) {
     {
         let x = control.value[i];
 
-        if (x == '(' || x == '[' || x == '{')
+        if (x == '{')
         {
 
-            // Push the element in the stack
+            // Push the elem
             stack.push(x);
             continue;
         }
 
-        // If current character is not opening
-        // bracket, then it must be closing.
-        // So stack cannot be empty at this point.
-        if (stack.length == 0)
-            return {invalid: true};
-
         let check;
         switch (x){
-        case ')':
-            check = stack.pop();
-            if (check == '{' || check == '[')
-            return {invalid: true};
-            break;
-
         case '}':
             check = stack.pop();
-            if (check == '(' || check == '[')
-            return {invalid: true};
-            break;
-
-        case ']':
-            check = stack.pop();
-            if (check == '(' || check == '{')
-            return {invalid: true};
-            break;
+            break
         }
     }
 
@@ -54,3 +31,54 @@ export function ValidatePath(control: AbstractControl) {
       return {invalid: true}
     }
 }
+
+// export function ValidatePath(control: AbstractControl) {
+//   let stack = [];
+
+//   // Traversing the Expression
+//   for(let i = 0; i < control.value.length; i++)
+//   {
+//       let x = control.value[i];
+
+//       if (x == '{')
+//       {
+
+//           // Push the elem
+//           stack.push(x);
+//           continue;
+//       }
+//       // If current character is not opening
+//       // bracket, then it must be closing.
+//       // So stack cannot be empty at this point.
+//       if (stack.length == 0)
+//           return {invalid: true};
+
+//       let check;
+//       switch (x){
+//       case ')':
+//           check = stack.pop();
+//           if (check == '{' || check == '[')
+//           return {invalid: true};
+//           break;
+
+//       case '}':
+//           check = stack.pop();
+//           if (check == '(' || check == '[')
+//           return {invalid: true};
+//           break;
+
+//       case ']':
+//           check = stack.pop();
+//           if (check == '(' || check == '{')
+//           return {invalid: true};
+//           break;
+//       }
+//   }
+
+//   // Check Empty Stack
+//   if (stack.length == 0){
+//     return null;
+//   }else{
+//     return {invalid: true}
+//   }
+// }
